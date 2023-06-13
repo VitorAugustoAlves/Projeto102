433852c30723f56a663832d8a3932b17226814f9
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2023-06-13 10:18:57 -0300

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir = "C:\Users\tamir\Downloads"
+to_dir = "C:\Users\tamir\OneDrive\Área de Trabalho\Arquivos_Documentos"
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+loop for in list_of_files():
+os.path.splitext()
+print(list_of_files)
